a320b937c91e2eab3083a5eb306413b08943c4d6
Chi Dang <dangtanminhchi@gmail.com>
2021-03-07 23:49:46 +0700

    Add: React top progress bar

--- src/layout/Dashboard.js
+++ src/layout/Dashboard.js
@@ -1,15 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import Topbar from './partials/TopBar';
 import LeftSidebar from './partials/LeftSidebar';
 import RightSidebar from './partials/RightSidebar';
 import SettingContext from '../context/setting-context';
 import Footer from './partials/Footer';
+import LoadingBar from 'react-top-loading-bar'
+
 
 const Dashboard = props => {
+    const [progress, setProgress] = useState(0)
     const settingContext = useContext(SettingContext);
     const dataThemeColor = `app-theme-${settingContext.themeColor.name}`;
+
+    useEffect(() => {
+        setProgress(100)
+    }, []);
+
     return (
         <>
+            <LoadingBar
+                color='#fff'
+                progress={progress}
+                onLoaderFinished={() => setProgress(0)}
+            />
             <div className="main-wrapper"
                 data-theme-sidebar-shrinked={settingContext.leftSidebarShrinked}
                 data-theme-mode={settingContext.themeMode}
