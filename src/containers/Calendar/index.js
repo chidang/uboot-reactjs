5bea92dd68d7bfb0533958cac8c0900c51f3c626
Chi Dang <dangtanminhchi@gmail.com>
2021-03-23 22:42:55 +0700

    Update calendar page

--- src/containers/Calendar/index.js
+++ src/containers/Calendar/index.js
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
 import CompCalendar from "./CompCalendar";
 
 const Calendar = () => {
     return <>
-        <CompCalendar />
+        <nav aria-label="breadcrumb">
+            <ol className="breadcrumb ps-0 fs-base">
+                <li className="breadcrumb-item"><Link to="/">Uboot</Link></li>
+                <li className="breadcrumb-item active" aria-current="page">Calendar</li>
+            </ol>
+        </nav>
+        <div className="header mb-4">
+            <h1 className="header-title h3">
+            <i className="bi-calendar4 text-primary" /> Calendar
+            </h1>
+        </div>
+        <div className="row">
+            <div className="col-12">
+                <CompCalendar />
+            </div>
+        </div>
     </>
 }
 
